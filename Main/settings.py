7b96cc2fadf0296474b1635d4e5733413b897d01
Sujalsurya <105475892+SujalSuryawanshi@users.noreply.github.com>
2024-10-03 17:57:19 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -84,7 +84,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 ROOT_URLCONF = 'Main.urls'
